146d8910bc29153f39ffe4e125a73d21194fd459
iam-zeenat-khan <92390852+iam-zeenat-khan@users.noreply.github.com>
2024-09-19 15:58:17 +0530

    Create 01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = '12345'
+accountCity =  "JAipur"
+let acountState;
+
+//accountId = 2 // not allowed
+/*
+
+prefer not to used var 
+because of isuse in block scope and functional scope
+
+
+*/ 
+accountEmail = 'hc@hc.com'
+accountPassword ="21212121"
+acountCity =" Indore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity , acountState])
