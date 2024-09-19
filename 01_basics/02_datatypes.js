9d34fc24763518e5691f281bf898ca87db7c72de
iam-zeenat-khan <92390852+iam-zeenat-khan@users.noreply.github.com>
2024-09-19 15:58:52 +0530

    Create 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use Strict";//treat all js code as newer version
+//alert(3+3);// we are using node js not browser
+let name ="zee"
+let age = 18
+let isloggedIn = false
+
+/*
+number =>2 to power 53
+bigint
+string =>""
+boolean =>true/false
+*/
+//null = standalone value
+//undefined 
+//symbol
+//object
+console.log(typeof null );//null
+console.log(typeof undefined);;//undefined
